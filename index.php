033e4a14ae2f862f8f69a86ec511fd38d812c578
a19carlosdf <a19carlosdf@iessanclemente.net>
2021-03-03 23:55:40 +0100

    Ignorando carpeta logs

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+<form method="post" action="" name="sigin-form">
+    <div class="form-element">
+        <label>Username</label>
+        <input type="text" name="username" required />
+    </div>
+    <div class="form-element">
+        <label>Password</label>
+        <input type="password" name="password" required />
+    </div>
+    <button type="submit" name="login" value="login">Login</button>
+</form>
+
